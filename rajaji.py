066aedbeac48320698e9e1e3d987d0915b7dbd0a
pramod74567i <shra.p912@gmail.com>
2024-12-07 21:54:58 +0530

    Update rajaji.py

--- rajaji.py
+++ rajaji.py
@@ -2,13 +2,13 @@
 #SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7662236939:AAHQZpfOzlAwJNodIt2i9QDAGkL_8zMbyvo")
+BOT_TOKEN=("7206133437:AAEZdHkDW4TSvR7lmfJb4FMFCJgOqzKvTW4")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@MR_CHETAN_OP")
+OWNER_USERNAME=("@rajaraj_04")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("904651079")
+ADMIN_IDS=("7855020275")
 
 
 
