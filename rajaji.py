e28b69cd54f0050cf99f8dd73a124bf6bb6da43f
pramod74567i <shra.p912@gmail.com>
2024-12-07 21:49:35 +0530

    Add files via upload

---
+++ rajaji.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7662236939:AAHQZpfOzlAwJNodIt2i9QDAGkL_8zMbyvo")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@MR_CHETAN_OP")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("904651079")
+
+
+
+
+
+  
